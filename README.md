492a83c571092a6af697da29422f3bf002a9edf8
Frederik Creemers <frederikcreemers@gmail.com>
2015-07-01 19:27:49 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+this is Podato's image serving app, built on top of Google App Engine. If you want to contribute, ut you're not sure how, just let me know in the Assembly (see below) chat.
+
+# Podato
+
+<a href="https://assembly.com/podato/bounties?utm_campaign=assemblage&utm_source=podato&utm_medium=repo_badge"><img src="https://asm-badger.herokuapp.com/podato/badges/tasks.svg" height="24px" alt="Open Tasks" /></a>
+
+## Social podcasts
+
+This is a product being built by the Assembly community. You can help push this idea forward by visiting [https://assembly.com/podato](https://assembly.com/podato).
+
+### How Assembly Works
+
+Assembly products are like open-source and made with contributions from the community. Assembly handles the boring stuff like hosting, support, financing, legal, etc. Once the product launches we collect the revenue and split the profits amongst the contributors.
+
+Visit [https://assembly.com](https://assembly.com) to learn more.
---
+++ main.py
@@ -0,0 +1,45 @@
+import urllib
+
+from google.appengine.api import app_identity
+from google.appengine.api import memcache
+from google.appengine.api import images
+from google.appengine.api import blobstore
+
+import webapp2
+import cloudstorage
+
+GCS_BUCKET = app_identity.get_default_gcs_bucket_name()
+
+def create_file(filename, contents, mime_type):
+  filename = "/" + GCS_BUCKET + "/" + filename
+  # Create a GCS file with GCS client.
+  with cloudstorage.open(filename, 'w', content_type=mime_type) as f:
+    f.write(contents.read())
+
+  # Blobstore API requires extra /gs to distinguish against blobstore files.
+  blobstore_filename = '/gs' + filename
+  # This blob_key works with blobstore APIs that do not expect a
+  # corresponding BlobInfo in datastore.
+  return blobstore.create_gs_key(blobstore_filename)
+
+
+def fetch_and_store(url):
+    res = urllib.urlopen(url)
+    mime = res.headers["content-type"]
+    if mime not in ["image/jpeg", "image/png", "image/gif"]:
+        raise ValueError("Unsupported image type: %s" % (mime))
+    return create_file(url, res, mime)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self, size):
+        url = self.request.get("url")
+        key = memcache.get(url)
+        if not key:
+            key = fetch_and_store(url)
+            memcache.set(url, key)
+        img_url = images.get_serving_url(key, secure_url=True, size=int(size))
+        self.redirect(img_url, permanent=True)
+
+app = webapp2.WSGIApplication([
+    webapp2.Route("/<size>", MainHandler)
+], debug=True)
